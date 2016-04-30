6a5c3f8dd179f57203a27a6451e03f5ccdc8442f
Luis Manuel Ramirez Vargas <luis.manuel.ramirez.91@gmail.com>
2016-04-30 08:50:34 -0500

    underscores init

---
+++ footer.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * The template for displaying the footer.
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package Simple_Blog
+ */
+
+?>
+
+	</div><!-- #content -->
+
+	<footer id="colophon" class="site-footer" role="contentinfo">
+		<div class="site-info">
+			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'simple-blog' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'simple-blog' ), 'WordPress' ); ?></a>
+			<span class="sep"> | </span>
+			<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'simple-blog' ), 'simple-blog', '<a href="http://www.luismdeveloper.com/" rel="designer">Luis Manuel Ramirez Vargas</a>' ); ?>
+		</div><!-- .site-info -->
+	</footer><!-- #colophon -->
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
